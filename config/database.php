3345e2d3f648db78a716689c8034e22e979b56ac
Artur <ttuca123@gmail.com>
2017-11-30 15:49:37 -0300

    app

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'sql10.freemysqlhosting.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'ibrem'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'sql10208162'),
+            'username' => env('DB_USERNAME', 'sql10208162'),
+            'password' => env('DB_PASSWORD', 'dEW6BdMhF9'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
